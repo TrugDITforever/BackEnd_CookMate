d7224094dc9491be0dc1e8a5722feafc4db00221
TrugDITforever <dotrung1512@gmail.com>
2025-11-30 18:42:34 +0700

    feat: logic create conversation

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ mongoose.connect(uri).then(() => {
 });
 const server = http.createServer(app);
 const io = socket(server);
-server.listen(8080, () => {
-  console.log("Server is running on port 8080");
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
 });
